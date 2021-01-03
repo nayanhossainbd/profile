9c0345e0598651f42b845cae9e940e8c1e04b969
Nayan Hossain <nayan78934@gmail.com>
2021-01-03 22:10:40 +0600

    Add react

--- dist/js/scripts.js
+++
@@ -1,1 +0,0 @@
-var mixer = mixitup('.projects_mixer');
--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import react from 'react';
 import './App.css';
 import Header from "./components/header";
+import About from './components/about';
 function App() {
   return (
     <div className="App">
     < Header />
+    <About />
     </div>
   );
 }
